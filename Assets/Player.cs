68b00689e837a3a71217f4c2e4243d11a7d238f8
NiamNjam <nida.kirdulyte@gmail.com>
2024-05-22 20:05:21 +0300

    Potions Blue Red Green (raycast)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,22 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     public FirstPersonMovement speedComponent;
+    public Jump jumpComponent;
+    public UnityEvent onDrink;
+    public GameObject equipText;
+    public Camera camera;
+    public LayerMask potionLayer;
     // Start is called before the first frame update
     void Start()
     {
         speedComponent = GetComponent<FirstPersonMovement>();
+        print("start");
     }
+    void Update()
+    {
+        var cam = camera.transform;
+        var collided = Physics.Raycast(cam.position, cam.forward, out var hit, 2f, potionLayer);
+        equipText.SetActive(collided);
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (collided && hit.transform.gameObject.name == "melynas")
+            {
+                Speed();
+                Destroy(hit.transform.gameObject);
 
+            }
+            if (collided && hit.transform.gameObject.name == "raudonas")
+            {
+                Jump();
+                Destroy(hit.transform.gameObject);
+
+            }
+            if (collided && hit.transform.gameObject.name == "zalias")
+            {
+                SpeedSlow();
+                Destroy(hit.transform.gameObject);
+
+            }
+            //onDrink.Invoke();
+            //Destroy(this.gameObject);
+        }
+        //if (potion == null) return;
+    }
     public void Speed()
     {
-        StartCoroutine(ExampleCoroutine());
+        StartCoroutine(SpeedCoroutine());
     }
 
-    IEnumerator ExampleCoroutine()
+    IEnumerator SpeedCoroutine()
     {
         //Print the time of when the function is first called.
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
@@ -24,6 +60,40 @@ public class Player : MonoBehaviour
         //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(2);
         speedComponent.speed = 5;
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+    public void SpeedSlow()
+    {
+        StartCoroutine(SpeedSlowCoroutine());
+    }
+
+    IEnumerator SpeedSlowCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        speedComponent.speed = 2;
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+        speedComponent.speed = 5;
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+    }
+
+    public void Jump()
+    {
+        StartCoroutine(JumpCoroutine());
+    }
+
+    IEnumerator JumpCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        jumpComponent.jumpStrength = 5;
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(4);
+        jumpComponent.jumpStrength = 2;
         //After we have waited 5 seconds print the time again.
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
--- Assets/Potions.cs
+++ Assets/Potions.cs
@@ -5,23 +5,48 @@ using UnityEngine.Events;
 
 public class Potions : MonoBehaviour
 {
+    /*
     public UnityEvent onDrink;
-    
-
+    public GameObject equipText;
+    public Camera camera;
+    public GameObject player;
     void Start()
     {
         
         print("start");
     }
+    
+    void Update()
+    {
+        var cam = camera.transform;
+        var collided = Physics.Raycast(cam.position, cam.forward, out var hit, 2f);
+        equipText.SetActive(collided && !player);
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (player == null && collided)
+            {
+                onDrink.Invoke();
+                Destroy(this.gameObject);
 
+            }
+            //onDrink.Invoke();
+            //Destroy(this.gameObject);
+        }
+        if (player == null) return;
+    }
+    /*
     void OnCollisionEnter(Collision other)
     {
+        
         if (other.gameObject.CompareTag("Player"))
         {
+            potionPic = true;
             onDrink.Invoke();
             Destroy(this.gameObject);
         }
     }
+    */
 
-    
+
+
 }
